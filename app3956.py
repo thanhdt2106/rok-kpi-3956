1c726720f9513a7a0edf20112f92ffa69156b018
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2026-09-23 03:55:07 +0700

    Update app3956.py

--- app3956.py
+++ app3956.py
@@ -220,7 +220,7 @@ st.markdown("""
 def load_data():
     try:
         sheet_id = "1l-PTL4YNhcD36Mwb_XDj5mF7GFfgrFm0fy4coBMCjXo"
-        gid1 = "400481050"
+        gid1 = "1324995926"
         gid2 = "1611380218"
         
         url1 = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid1}'
