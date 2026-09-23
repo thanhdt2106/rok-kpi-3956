f41aa7a44614c0153e6f350a442f4034c3d2d41b
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2026-09-23 12:25:43 +0700

    Update app3956.py

--- app3956.py
+++ app3956.py
@@ -221,7 +221,7 @@ def load_data():
     try:
         sheet_id = "1l-PTL4YNhcD36Mwb_XDj5mF7GFfgrFm0fy4coBMCjXo"
         gid1 = "1324995926"
-        gid2 = "1611380218"
+        gid2 = "1374261701"
         
         url1 = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid1}'
         url2 = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid2}'
